bd4deb425dc571317e745e384280d6daf0a2e0d9
Aleksandra Kierska <A.W.Kierska@student.tudelft.nl>
2024-04-28 14:18:46 +0200

    Add file retrieval methods to FileReader

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,12 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileReader {
 	private String directoryPath;
+	File[] files;
 
 	public FileReader (String directoryPath) {
 		this.directoryPath = directoryPath;
 	}
 
-	public void getNextMethod() {
+	private File[] getFiles () throws SecurityException, IOException {
+		if (files == null) {
+			files = getAllAnalysableFiles();
+		}
+		return files;
+	}
 
+	private File[] getAllAnalysableFiles () throws SecurityException, IOException {
+		Path dir = Paths.get(directoryPath);
+
+		return Files.walk(dir)
+				       .filter(path -> Files.isRegularFile(path) && isAnalysableFileType(path))
+				       .map(Path::toFile)
+				       .toArray(File[]::new);
+
+	}
+
+	/*
+	 * gets the next Java/Kotlin fileName in the directory, now getNextMethod, will return methods from that file
+	 * */
+	public String getNextFileName () {
+		return null;
+	}
+
+	public String getNextMethod () {
+		return null;
+	}
+
+	private static boolean isAnalysableFileType (Path filePath) {
+		return (filePath.endsWith(".java") || filePath.endsWith(".kt") || filePath.endsWith(".kts"));
 	}
 
 }
