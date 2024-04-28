f6e3d292e5e653e799514ae3b7bfedf788280553
Aleksandra Kierska <A.W.Kierska@student.tudelft.nl>
2024-04-28 14:28:11 +0200

    src/main/java/FileReader.java

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -7,9 +7,13 @@ import java.nio.file.Paths;
 public class FileReader {
 	private String directoryPath;
 	File[] files;
+	String[] methods;
+	int filesIterator = 0;
+	int methodIterator = 0;
 
 	public FileReader (String directoryPath) {
 		this.directoryPath = directoryPath;
+		filesIterator = 0;
 	}
 
 	private File[] getFiles () throws SecurityException, IOException {
@@ -28,20 +32,26 @@ public class FileReader {
 				       .toArray(File[]::new);
 
 	}
-
+	private static boolean isAnalysableFileType (Path filePath) {
+		return (filePath.endsWith(".java") || filePath.endsWith(".kt") || filePath.endsWith(".kts"));
+	}
 	/*
 	 * gets the next Java/Kotlin fileName in the directory, now getNextMethod, will return methods from that file
 	 * */
 	public String getNextFileName () {
-		return null;
+		if(files.length < filesIterator) {
+			methods = getMethodStringsFromFile(files[filesIterator]);
+			methodIterator = 0;
+			return files[filesIterator++].getName();
+		}
+		else return null;
+	}
+
+	private String[] getMethodStringsFromFile (File file) {
+		return new String[0];
 	}
 
 	public String getNextMethod () {
-		return null;
+		return (methodIterator < methods.length) ? methods[methodIterator++] : null;
 	}
-
-	private static boolean isAnalysableFileType (Path filePath) {
-		return (filePath.endsWith(".java") || filePath.endsWith(".kt") || filePath.endsWith(".kts"));
-	}
-
 }
