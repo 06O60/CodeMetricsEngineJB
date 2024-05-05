d3088fd88a4f9a97e5955706a3d43d07048c6ec0
Aleksandra Kierska <A.W.Kierska@student.tudelft.nl>
2024-05-05 15:03:47 +0200

    Add functionality to report Code style and complexity analysis to CodeAnalyzer

--- src/main/java/CodeAnalyzer.java
+++ src/main/java/CodeAnalyzer.java
@@ -1,9 +1,17 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CodeAnalyzer {
 
 	private FileReader fileReader;
+	private int numOfResults;
+
+	public CodeAnalyzer (String directoryPath,int numOfResults) {
+		this.fileReader = new FileReader(directoryPath);
+		this.numOfResults = numOfResults;
+	}
+
 	//maybe add a possibility to set flags, as to what kind of checks do we want to be performed?
 	//so the fields would be flags set
 
@@ -12,14 +20,14 @@ public class CodeAnalyzer {
 	 * or should I just construct it with directoryPath and make it possible to set it?
 	 * should it return string of analysis, or print it ?
 	 */
-	public String analyze (String directoryPath) {
+	public String analyze () {
 		StringBuilder stringBuilder = new StringBuilder();
 		try {
-			fileReader = new FileReader(directoryPath);
 			String fileName = fileReader.getNextFileName();
 			while (fileName != null) {
 				stringBuilder.append(String.format("Code analysis of file %s:\n", fileName));
 				stringBuilder.append(analyzeFile());
+				stringBuilder.append("\n");
 				fileName = fileReader.getNextFileName();
 			}
 			return stringBuilder.toString();
@@ -28,22 +36,50 @@ public class CodeAnalyzer {
 		} catch (SecurityException e) {
 			//
 		}
-		return "";
+		return stringBuilder.toString();
 	}
 
 	/*
 	 * It would go through all the methods in the file and do the analysis, would be used by analyze
 	 * */
 	private String analyzeFile () {
-		StringBuilder stringBuilder = new StringBuilder();
-		Function methodToAnalyze = fileReader.getNextFunction();
+		List<Function> functionsToAnalyze = new ArrayList<>();
+		Function functionToAnalyze = fileReader.getNextFunction();
 
-		while (methodToAnalyze != null) {
-		//	List<Pair<String, Integer>> complexityEvaluationResult = CodeComplexityAnalyzer.evaluateComplexity(methodToAnalyze, 3);
-			//Pair<String, Integer> codeStyleEvaluationResult =
-		//	stringBuilder.append(complexityEvaluationResult);
-			methodToAnalyze = fileReader.getNextFunction();
+		while (functionToAnalyze != null) {
+			functionsToAnalyze.add(functionToAnalyze);
+			functionToAnalyze = fileReader.getNextFunction();
 		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+		List<Pair<String, Integer>> codeComplexityResult = CodeComplexityAnalyzer.evaluateComplexity(
+				functionsToAnalyze,
+				numOfResults
+		);
+
+		stringBuilder.append(
+				String.format("========== Code complexity (showing top %d results) ==========\n", numOfResults));
+		if(codeComplexityResult.isEmpty())
+			stringBuilder.append("No methods to perform code complexity check found.");
+		int resCount = 1;
+		for (Pair<String, Integer> p : codeComplexityResult) {
+			stringBuilder.append(String.format("%d. Function name: %s, Code complexity value: %d\n",
+			                                   resCount++,
+			                                   p.first(),
+			                                   p.second())
+			);
+		}
+		stringBuilder.append("\n");
+
+		stringBuilder.append("========== Code style ==========\n");
+		try {
+			stringBuilder.append(
+					String.format("Percentage of methods that do not comply with the naming conventions: %.2f%%",
+					              CodeStyleAnalyzer.evaluateCodeStyle(functionsToAnalyze)));
+		} catch(IllegalArgumentException e) {
+			stringBuilder.append("No methods to perform code style check found.");
+		}
+		stringBuilder.append("\n");
 		return stringBuilder.toString();
 	}
 }
