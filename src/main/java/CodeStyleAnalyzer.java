ef26ab4dda16594866d38c1af9f0f7f79e9bf18b
Aleksandra Kierska <A.W.Kierska@student.tudelft.nl>
2024-05-05 15:48:17 +0200

    Add javadocs to CodeStyleAnalyzer and improve the formatting

--- src/main/java/CodeStyleAnalyzer.java
+++ src/main/java/CodeStyleAnalyzer.java
@@ -2,20 +2,33 @@ import java.util.List;
 
 public class CodeStyleAnalyzer {
 
+	/**
+	 * Evaluates the overall code style compliance of a list of functions.
+	 *
+	 * @param functions The list of functions to evaluate.
+	 * @return The percentage of non-compliant methods in the list.
+	 * @throws IllegalArgumentException If the list of functions is empty.
+	 */
 	public static double evaluateCodeStyle(List<Function> functions) {
-		int nonCompliantMethods = 0;
-		for(Function f: functions)
-			if(!evaluateCodeStyleOfAMethod(f).second())
-				nonCompliantMethods++;
 		if(functions.isEmpty())
 			throw new IllegalArgumentException("No methods to evaluate");
-		return (100.0*nonCompliantMethods)/functions.size();
+
+		int nonCompliantMethods = 0;
+		for(Function f: functions)
+			if(! evaluateCodeStyleOfAFunction(f).second())
+				nonCompliantMethods++;
+
+		return (100.0 * nonCompliantMethods) / functions.size();
 	}
 
-	//1. it has to be in camelcase
-	//2. no spaces
 	//TODO: 3. only constructor starts with upper case
-	public static Pair<String, Boolean> evaluateCodeStyleOfAMethod(Function methodToAnalyze) {
-		return new Pair(methodToAnalyze.name(), methodToAnalyze.name().matches("[a-zA-Z][a-zA-Z0-9]*"));
+	/**
+	 * Evaluates the code style compliance of a single function.
+	 * Currently it checks if the function name is in camelCase and doesn't contain illegal characters such as "_"
+	 * @param functionToAnalyze The function to analyze.
+	 * @return A Pair containing the method name and a boolean indicating compliance.
+	 */
+	public static Pair<String, Boolean> evaluateCodeStyleOfAFunction (Function functionToAnalyze) {
+		return new Pair(functionToAnalyze.name(), functionToAnalyze.name().matches("[a-zA-Z][a-zA-Z0-9]*"));
 	}
 }
