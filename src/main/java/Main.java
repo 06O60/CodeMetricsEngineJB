69aca4a39864d0f0489c1a40332fb8522de213f2
Aleksandra Kierska <A.W.Kierska@student.tudelft.nl>
2024-05-05 20:16:59 +0200

    Improve readability of error messages in Main by adding colors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
+	private static String ANSI_RESET = "\u001B[0m";
+	private static String ANSI_RED = "\u001B[31m";
 	public static void main (String[] args) {
 		Scanner userInputScanner = new Scanner(System.in);
 
@@ -15,11 +16,11 @@ public class Main {
 				System.out.println(codeAnalyzer.analyzeDirectory());
 				return;
 			} catch (IOException e) {
-				System.out.println("An IOException occured while trying to read the directory, please try again.");
+				System.out.println(ANSI_RED + "An IOException occured while trying to read the directory, please try again." + ANSI_RESET);
 			} catch (SecurityException e) {
-				System.err.println("The security manager denied access to the directory, please try again");
+				System.err.println(ANSI_RED + "The security manager denied access to the directory, please try again" + ANSI_RESET);
 			} catch (Exception e) {
-				System.err.println("Something went wrong... please try again.");
+				System.err.println(ANSI_RED + "Something went wrong... please try again." + ANSI_RESET);
 			}
 		}
 	}
